2fe785ed8622009630b07f817f71e553059239e1
pcarlip <pcarlip@ucsd.edu>
2018-09-13 19:28:15 -0700

    change outputs to imperial

--- Design/Vehicle-Sizing/unit_conversions.py
+++ Design/Vehicle-Sizing/unit_conversions.py
@@ -9,3 +9,18 @@ def lbfToN(lbf):
 
 def ftToM(ft):
     return ft / 3.28084
+
+def mToFt(m):
+    return m * 3.28084
+
+def kmToMi(km):
+    return km / 1.60934
+
+def paToPsi(pa):
+    return pa / 6894.76
+
+def kgm3ToLbft3(kgm3):
+    return kgm3 / 16.0185
+
+def nToLbf(n):
+    return n / 4.44822
